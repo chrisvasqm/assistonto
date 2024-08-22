e2885681e7f2478efdda5758f81ecc3d29f41636
chrisvasqm <christianvasquezmelo@gmail.com>
2024-08-22 18:39:15 -0400

    Add GET appointment endpoint

---
+++ backend/routers/appointment.ts
@@ -0,0 +1,52 @@
+import {Router, Response, Request} from 'express';
+import prisma from '../prisma/database';
+import dayjs from 'dayjs';
+
+const router = Router();
+
+interface AppointmentRequest extends Request {
+  query: {
+    date: string;
+  };
+}
+
+router.get('/', async (request: AppointmentRequest, response: Response) => {
+  const slots = [
+    {time: '08:00'},
+    {time: '09:00'},
+    {time: '10:00'},
+    {time: '11:00'},
+    {time: '12:00'},
+    {time: '13:00'},
+    {time: '14:00'},
+    {time: '15:00'},
+    {time: '16:00'},
+    {time: '17:00'}
+  ];
+
+  const date = dayjs(request.query.date).startOf('day').toDate();
+  const endOfDay = dayjs(date).endOf('day').toDate();
+
+  const appointments = await prisma.appointment.findMany({
+    where: {
+      time: {
+        gte: date,
+        lt: endOfDay
+      },
+      isBooked: true
+    }
+  });
+
+  const availability = slots.map(slot => {
+    const appointmentTime = dayjs(`${request.query.date}T${slot.time}:00`).toDate();
+    const isBooked = appointments.some(
+      appointment => appointment.time.getTime() === appointmentTime.getTime()
+    );
+
+    return {...slot, isBooked};
+  });
+
+  response.send(availability);
+});
+
+export default router;
--- backend/startup/routes.ts
+++ backend/startup/routes.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import homeRouter from '../routers/home';
+import appointmentRouter from '../routers/appointment';
 
 export default function routes(app: Express) {
   app.use('/api', homeRouter);
+  app.use('/api/appointments', appointmentRouter);
 }
